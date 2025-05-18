42f689d49d76e9a153226288d4d9038445cfdb05
Rajat Pathania <83996978+pathania0001@users.noreply.github.com>
2025-05-18 19:06:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ var minutes = document.querySelector(".min-hand");
 var hours = document.querySelector(".hour-hand");
 function update() {
 var currTime = new Date();
-var hour = (currTime.getHours()%12)*3600+3*60*60;
-var second = currTime.getSeconds()+15;
-var minute = (currTime.getMinutes()%60)*60+15*60;
+var hour = (currTime.getHours()%12)*3600;
+var second = currTime.getSeconds();
+var minute = (currTime.getMinutes()%60)*60;
 	hours.style.transform=`rotate(${(360/(3600*12))*hour}deg)`;
 	minutes.style.transform=`rotate(${(1/10)*minute}deg)`;
 	seconds.style.transform=`rotate(${second*6}deg)`;

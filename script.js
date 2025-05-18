a2e1a25e5eb7edb81d4a056a8bb13f362a21d87f
Rajat Pathania <83996978+pathania0001@users.noreply.github.com>
2025-05-18 19:28:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
-var seconds = document.querySelector(".second-hand");
-var minutes = document.querySelector(".min-hand");
-var hours = document.querySelector(".hour-hand");
+const seconds = document.querySelector(".second-hand");
+const minutes = document.querySelector(".min-hand");
+const hours = document.querySelector(".hour-hand");
+
 function update() {
-var currTime = new Date();
-var hour = (currTime.getHours()%12)*3600+3*3600;
-var second = currTime.getSeconds()+15;
-var minute = (currTime.getMinutes()%60)*60+15*60;
-	hours.style.transform=`rotate(${(360/(3600*12))*hour}deg)`;
-	minutes.style.transform=`rotate(${(1/10)*minute}deg)`;
-	seconds.style.transform=`rotate(${second*6}deg)`;
+  const now = new Date();
+
+  const second = now.getSeconds();
+  const minute = now.getMinutes();
+  const hour = now.getHours() % 12;
+
+  const secondDeg = second * 6 + 15; // 360 / 60
+  const minuteDeg = minute * 6 + second * 0.1 + 90; // 360 / 60 + progress
+  const hourDeg = hour * 30 + minute * 0.5 + 3;     // 360 / 12 + progress
+
+  seconds.style.transform = `rotate(${secondDeg}deg)`;
+  minutes.style.transform = `rotate(${minuteDeg}deg)`;
+  hours.style.transform = `rotate(${hourDeg}deg)`;
 }
-setInterval(update,1000)
+
+setInterval(update, 1000);
 update();

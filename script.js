cf4e63cef1565b3c4896467dceca52d020952aad
Rajat Pathania <83996978+pathania0001@users.noreply.github.com>
2025-07-24 12:18:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ function update() {
   const hour = now.getHours() % 12;
 	console.log(second,hour)
   const secondDeg = (second+15)*6; // 360 / 60
-  const minuteDeg = (minute+15) * 6 + second * 0.1 ; // 360 / 60 + progress
-  const hourDeg = (hour+3) * 30 + minute * 0.5 + second*(30/3600);     // 360 / 12 + progress
+  const minuteDeg = (minute+15) * 6 + (second+15) * 0.1 ; // 360 / 60 + progress
+  const hourDeg = (hour+3) * 30 + (minute+15) * 0.5 + (second+15)*(30/3600);     // 360 / 12 + progress
 
   seconds.style.transform = `rotate(${secondDeg}deg)`;
   minutes.style.transform = `rotate(${minuteDeg}deg)`;
